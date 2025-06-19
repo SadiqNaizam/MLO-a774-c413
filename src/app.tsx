286acb78078eb7e2e302ecf51bac91542857edda
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-19 16:53:02 +0530

    Updated code files: app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,39 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import DashboardPage from "./pages/DashboardPage";
+import NotFound from "./pages/NotFound"; // Assuming NotFound.tsx exists
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  console.log('App loaded');
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} /> {/* Default to LoginPage */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/dashboard/*" element={<DashboardPage />} /> {/* Nested routes for dashboard handled within DashboardPage or sub-routes */}
+            
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} /> {/* Always Include This Line As It Is. */}
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
